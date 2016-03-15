0bdf862ef1c8d4901160a2b050ca0fe9eb70cdc2
Jeremy Wright <jwright@itracs.com>
2016-03-15 14:46:28 -0700

    Adding simple function exported to native code.

---
+++ src/Ciphers.Test/Caesar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Test
+{
+    [TestClass]
+    public class Caesar
+    {
+        [TestMethod]
+        public void CipherTest()
+        {
+            string ciphertext = Ciphers.Caesar.Cipher("Hello World");
+            Assert.AreEqual(ciphertext, "UryybJbeyq");
+        }
+    }
+}
